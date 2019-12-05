0dde569218797083dd9b974eb15e756cc1ff1ddf
w3cj <cj@null.computer>
2019-12-05 08:46:20 -0700

    :scissors: Remove nedb; Just use in memory db;

--- src/db.js
+++ src/db.js
@@ -1,7 +1,27 @@
-const Datastore = require('nedb-promises')
-const db = new Datastore({
-  filename: 'bot.db',
-  autoload: true
-});
+const data = {};
 
-module.exports = db;
+async function update({ _id }, doc) {
+  if (doc.$set) {
+    const existing = data[_id];
+    if (existing) {
+      Object.keys(doc.$set).forEach(prop => {
+        existing[prop] = doc.$set[prop];
+      });
+    } else {
+      data[_id] = doc.$set;
+    }
+  } else {
+    data[_id] = doc;
+  }
+
+  return data[_id];
+}
+
+async function remove({ _id }) {
+  delete data[_id];
+}
+
+module.exports = {
+  update,
+  remove
+};
