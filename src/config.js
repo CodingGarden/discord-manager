eb797e8fbb93f5cac145cdfdaeff0e3df5210a83
KarateWumpus <48181821+KarateWumpus@users.noreply.github.com>
2020-07-21 10:33:15 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -15,13 +15,13 @@ module.exports = {
   MIN_INTRO_MESSAGE_LENGTH: process.env.MIN_INTRO_MESSAGE_LENGTH,
   WELCOME_MESSAGE: `Welcome to the 🌻 💚 **Coding Garden Discord!** 💚 🌻 
 
-Before gaining access to all of the channels, you will need to **read the rules and code of conduct** in the #welcome channel.
+Before gaining access to all of the channels, you will need to **read the rules and code of conduct** in the <#451779463819034626> channel.
 
-After you have **read and acknowledged** that you will abide by the rules and code of conduct, You will need to introduce yourself in the #introductions channel.
+After you have **read and acknowledged** that you will abide by the rules and code of conduct, You will need to introduce yourself in the <#451783474106335243> channel.
 
 **Your introduction must be a minimum of 40 characters long.** You can tell us your name, what you do, how long you've been coding, and maybe even your favorite food! Other fun things to talk about include: how you found Coding Garden or what your favorite thing about coding is.
 
-**REMEMBER:** **Do not spam multiple channels with the same message or you will be muted/banned.** Always spend at least 30 seconds to decide what channel is best for your comment/question. (See an overview of channels in the #welcome channel)
+**REMEMBER:** **Do not spam multiple channels with the same message or you will be muted/banned.** Always spend at least 30 seconds to decide what channel is best for your comment/question. (See an overview of channels in the <#451779463819034626> channel)
 
 Let's have some fun!`
 };
