81adf3548b9efed19c0b24302b7a968db72d12ef
Andreu Cotanda Oliver <cotoli23@gmail.com>
2020-08-17 17:02:27 +0200

    Fix 'modChannel is undefined' error

--- src/tasks/supportTickets.js
+++ src/tasks/supportTickets.js
@@ -5,30 +5,32 @@ const {
   SUPPORT_TICKET_MESSAGE,
 } = require('../config');
 
-const modChannel = guild.channels.cache.get(MOD_CHANNEL_ID);
+async function sendMessageToModsChannel(guild, message) {
+  const modChannel = guild.channels.cache.get(MOD_CHANNEL_ID);
+  modChannel.send(message);
+}
 
 async function listenForTickets(guild) {
   const ticketChannel = guild.channels.cache.get(TICKET_CHANNEL_ID);
   const ticketMessage = await ticketChannel.messages.fetch(TICKET_MESSAGE_ID);
-
   const collector = ticketMessage.createReactionCollector(() => true);
   collector.on('collect', async (reaction, user) => {
-    modChannel.send(`${user.tag} has opened a support ticket.`);
-    listenForUserDMs(user);
+    sendMessageToModsChannel(guild, `${user.tag} has opened a support ticket.`);
+    listenForUserDMs(guild, user);
     reaction.remove();
   });
 }
 
-async function listenForUserDMs(user) {
+async function listenForUserDMs(guild, user) {
   const userChannel = await user.createDM();
   const collector = userChannel.createMessageCollector(() => true, {
-    time: 86400,
+    time: 86400, // 24h
   });
 
   userChannel.send(SUPPORT_TICKET_MESSAGE);
 
   collector.on('collect', async (message) => {
-    modChannel.send(`${user.tag} sent: ${message}`);
+    sendMessageToModsChannel(guild, `${user.tag} sent: ${message}`);
   });
 }
 
